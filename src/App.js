5a59c2f91a99a14b2bcb197a01c48aa5c604f21c
JonathanBacklin <Jonbac020809@student.jenseneducation.se>
2021-12-09 10:36:26 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React,{useState, useEffect} from 'react'
+import axios from 'axios';
+
+import './App.css';
+import Coin from './Coin';
+
+
+
+
+
+function App() {
+ const [coins, SetCoins] = useState([])
+ const [search, setSearch] = useState('')
+
+  useEffect(() =>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=sek&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      SetCoins(res.data)
+    }).catch(error => console.log('Error with get request'))
+  })
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1>Hitta din favorit</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/>
+        </form>
+        
+        <div className="watchlist-container">
+            <h1>Watchlist</h1>
+        </div>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin 
+          key={coin.id}
+           name={coin.name} 
+           image={coin.image}
+           symbol={coin.symbol}
+           marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          
+          />
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Coin.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faStar} />
+
+
+const Coin = ({name,image,symbol,price,priceChange,marketcap, addToWatchlist }) => {
+    return (
+        <>
+        
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">{price}kr</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>)
+                        :(<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
+                    }
+                    <p className="coin-marketcap">
+                        {marketcap.toLocaleString()}kr
+                    </p>
+                    <button className="coin-watchlist-button" onClick={() => console.log('Click')}>{element}</button>
+                </div>
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default Coin
